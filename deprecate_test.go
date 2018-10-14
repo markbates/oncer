05fccaae8fc423476d98fd4c3e4699ba0fbbde48
Mark Bates <mark@markbates.com>
2018-10-14 15:46:34 -0400

    removed the file:line number for deprecate. it was often wrong, and possibly a security hole

--- deprecate_test.go
+++ deprecate_test.go
@@ -17,7 +17,6 @@ func Test_Deprecate_NoMessage(t *testing.T) {
 	Deprecate(4, "Test_Deprecate_NoMessage", "")
 	act := bb.String()
 	r.True(strings.HasPrefix(act, "[DEPRECATED] Test_Deprecate_NoMessage has been deprecated."))
-	r.Contains(act, "deprecate_test.go:")
 }
 
 func Test_Deprecate_Message(t *testing.T) {
@@ -29,6 +28,5 @@ func Test_Deprecate_Message(t *testing.T) {
 	Deprecate(4, "Test_Deprecate_Message", "Use something else instead")
 	act := bb.String()
 	r.True(strings.HasPrefix(act, "[DEPRECATED] Test_Deprecate_Message has been deprecated."))
-	r.Contains(act, "deprecate_test.go:")
 	r.Contains(act, "Use something else instead")
 }
